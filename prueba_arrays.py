ee293e1e51bba32328d7ac726412d23254b2490b
Eli Tamez <elitamez66@gmail.com>
2021-10-09 01:49:12 -0500

    Primeras pruebas arrays

---
+++ prueba_arrays.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+sbox=np.zeros((5,5))
+print(sbox)
+a=0
+
+for i in range(len(sbox)):
+    for j in range(len(sbox)):
+        sbox[i, j]=a
+        a+=1
+        
+print(sbox)
+print("\n")
+
+q=str(int(sbox[2][2]))
+print(q[1])
